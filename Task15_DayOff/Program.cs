46dae95b27e6c806f1b86558e4dcc093714a7acd
NataKuz <nv8282@mail.ru>
2022-10-18 22:31:34 +0800

    Task 15 to determine if a day is a holiday

---
+++ Task15_DayOff/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Enter day of the week number:");
+int dayOfWeek = int.Parse(Console.ReadLine()!);
+
+string DayOff(int number)
+{
+    string dayOff = "Yes, this day is a holiday";
+
+    if (number > 5 && number < 8)
+    {
+        dayOff = "Yes, this day is a holiday";
+    }
+    if (number >0 && number < 6)
+    {
+        dayOff = "No, this day is not a holiday";
+    }
+    if (number < 1 || number > 7)
+    {
+        dayOff = "Number is not a day of the week";
+    }
+    return dayOff;
+}
+
+string day = DayOff(dayOfWeek);
+Console.WriteLine(day);
